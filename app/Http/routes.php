b0f91b05ba997d472ae7ecd5417ccf7e6b4fd60d
Pamela Lim <pamelaliusm@gmail.com>
2017-05-24 10:25:59 +1000

    handle mobile

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,28 +40,17 @@ Route::resource('books', 'BookController');
 * Login routes
 */
 
-# Process logout
-Route::get('/login', function(){
-    return view('auth.login');
-});
-
-Route::get('/register', function(){
-    return view('auth.register');
-});
+# Process logout/login for mobile only
 
 Route::get('/logout', 'Auth\AuthController@logout');    
-Route::get('/show-login-status', function() {
 
-    # You may access the authenticated user via the Auth facade
-    $user = Auth::user();
+Route::get('/login', 'Auth\AuthController@getLogin');
 
-    if($user) {
-        echo 'You are logged in.';
-        dump($user->toArray());
-    } else {
-        echo 'You are not logged in.';
-    }
+# Process login form
+Route::post('/login', 'Auth\AuthController@postLogin');
 
-    return;
+# Show registration form
+Route::get('/register', 'Auth\AuthController@getRegister');
 
-});
+# Process registration form
+Route::post('/register', 'Auth\AuthController@postRegister');
